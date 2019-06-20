260d1af2861dbd91af1e3218ee237dfdffc420fb
saimasharleen <saimasharleen0@gmail.com>
2019-06-21 05:12:14 +0600

    Create admin.js

---
+++ controller/admin.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(request, response){
+	response.render('admin');
+});
+
+router.post('/', function(request, response){
+	/*response.send(request.body.username +"<br/>"+ request.body.password);*/
+});
+
+module.exports = router;
