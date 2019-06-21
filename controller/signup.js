89ed6334e1f0720e80ac483fcea47522953193ae
saimasharleen <saimasharleen0@gmail.com>
2019-06-21 17:19:18 +0600

    Update signup.js

--- controller/signup.js
+++ controller/signup.js
@@ -9,28 +9,42 @@ router.get('/', function(request, response){
 
 router.post('/', function(request, response){
 	/*response.send(request.body.username +"<br/>"+ request.body.password);*/
-	  console.log("test");
+	  
       var user = {
       	firstname : request.body.firstname,
       	lastname : request.body.lastname,
       	username : request.body.username,
       	email : request.body.email,
-      	phone : request.body.phoneno,
+      	phoneno : request.body.phoneno,
       	area : request.body.area,
-      	usertype : request.body.usertype
+      	usertype : request.body.usertype,
+            password : request.body.password,
+            image : "abcd",
+            status: "requested"
       };
       if(!user.firstname || !user.lastname || !user.username || !user.email || !user.phoneno || !user.area || !user.usertype){
-      	response.redirect('/signup');
+      	//console.log(user);
+            response.redirect('/signup');
       }else
-      {
-      	userModel.insert(user,function(status){
+      {         	
+            userModel.insert(user,function(status){
       		if(status == true){
-      			console.log(user.username);
-      			response.redirect('/login');
+                        userModel.insertLogin(user,function(status){
+                              if(status == true){
+                                    
+                                    response.redirect('/login');
+                              }
+                              else{
+                                    console.log(user);
+                                    response.redirect('/signup');
+                              }
+                        });
+      			//console.log(user.username);
       		}
       		else{
 
-      			response.send('Error in adding user');
+      			/*response.send('Error in adding user');*/
+                        response.redirect('/signup');
       		}
       	});
       }
