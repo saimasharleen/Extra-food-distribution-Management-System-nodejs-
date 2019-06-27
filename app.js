efc2f71495fe3e8a29779a6ec9c0055797e84ab8
NajiaBarota <43447008+NajiaBarota@users.noreply.github.com>
2019-06-27 16:01:58 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //DECLARATION
 var express 	= require('express');
 var bodyParser 	= require('body-parser');
-//var home 		= require('./controllers/home');
 var signup 		= require('./controller/signup');
+var login 		= require('./controller/login');
 var admin 		= require('./controller/admin');
+var volunteer 		= require('./controller/volunteer');
 var app = express();
 
 
@@ -13,22 +14,25 @@ app.use('/assets', express.static('assets'));
 
 
 //MIDDLEWARE
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//app.use(bodyParser.json());
 
 
 //ROUTING
 app.use('/signup', signup);
+app.use('/login', login);
 app.use('/admin', admin);
-/*app.use('/home', home);*/
-app.get('/', function(request, response){
-	/*response.send('Hello from expressJs');*/
+app.use('/volunteer', volunteer);
+app.get('/index', function(request, response){
 	response.render('index');
-});
+}); 
 
 
 //SERVER STARTUP
 app.listen(3000, function(){
-	console.log("Server startead at 3000...");
+	console.log("Server started at 3000...");
 });
-
 
