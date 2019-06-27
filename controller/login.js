70890ffd04069e9238c53b7dd9666fd4df2c36f0
NajiaBarota <43447008+NajiaBarota@users.noreply.github.com>
2019-06-27 15:52:06 +0600

    Create login.js

---
+++ controller/login.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var mysql = require('mysql');
+var userModel = require.main.require('./models/user-model');
+var router = express.Router();
+
+router.get('/', function(request, response){
+	response.render('login');
+});
+
+router.post('/', function(request, response){
+	/*response.send(request.body.username +"<br/>"+ request.body.password);*/
+ var user = {
+      	username : request.body.username,
+      	password : request.body.password,
+          
+      };
+      if(!user.username || !user.password ){
+      	//console.log(user);
+            response.redirect('/login');
+      }else
+      {
+      	userModel.validate(user,function(status){
+      		if(status == 'volunteer'){
+      			//console.log(user.username);
+      			response.redirect('/volunteer');
+      		}
+                  else if(status.usertype == 'admin' && status.status== 'accept'){
+                        //console.log(user.username);
+                        response.redirect('/admin');
+                  }
+                  else if(status == 'owner'){
+                        //console.log(user.username);
+                        response.redirect('/owner');
+                  }
+                  else if(status == 'eventmanager'){
+                        //console.log(user.username);
+                        response.redirect('/eventmanager');
+                  }
+                  else if(status == 'generaluser'){
+                        //console.log(user.username);
+                        response.redirect('/generaluser');
+                  }
+
+      		else{
+
+      			/*response.send('Error in adding user');*/
+                        response.redirect('/login');
+      		}
+      	});
+      }
+});
+
+module.exports = router;
