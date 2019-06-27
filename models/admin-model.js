09905ad3d6650faad1bc84ac04c210838f8851f2
NajiaBarota <43447008+NajiaBarota@users.noreply.github.com>
2019-06-27 15:51:25 +0600

    Create admin-model.js

---
+++ models/admin-model.js
@@ -0,0 +1,38 @@
+var db = require('./db');
+
+module.exports={
+
+	get: function(userId, callback){
+		var sql = "select * from user where id=?";
+		db.getResult(sql, [userId], function(result){
+			//console.log(result);
+			callback(result[0]);
+		});
+	},
+
+	getAll: function(callback){
+		var sql = "SELECT * FROM userLogin";
+		db.getResult(sql, [], function(result){
+			callback(result);
+		});
+	},
+
+	update: function(user, callback){
+		var sql = "UPDATE userLogin set status='reject' where username=?";
+
+		db.execute(sql, [user], function(status){
+			callback(status);
+		});
+	},
+
+updateAccept: function(user, callback){
+		var sql = "UPDATE userLogin set status='accept' where username=?";
+
+		db.execute(sql, [user], function(status){
+			callback(status);
+		});
+	},
+	
+	
+	
+}
