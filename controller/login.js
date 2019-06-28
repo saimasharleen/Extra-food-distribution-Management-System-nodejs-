fd963a66f4652c9bf391d760edf62e16e3ecb20f
NajiaBarota <43447008+NajiaBarota@users.noreply.github.com>
2019-06-28 08:12:37 +0600

    Update login.js

--- controller/login.js
+++ controller/login.js
@@ -4,10 +4,16 @@ var userModel = require.main.require('./models/user-model');
 var router = express.Router();
 
 router.get('/', function(request, response){
+      userModel.getNotice(function(status){
+                       response.render('index',{userList: status});  
+                        });
+      //response.render('index');
+});
+router.get('/login', function(request, response){
 	response.render('login');
 });
 
-router.post('/', function(request, response){
+router.post('/login', function(request, response){
 	/*response.send(request.body.username +"<br/>"+ request.body.password);*/
  var user = {
       	username : request.body.username,
