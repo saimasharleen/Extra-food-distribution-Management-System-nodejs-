c4c7ebcc66f202333ecdf6525bf73bdc079791c2
saimasharleen <saimasharleen0@gmail.com>
2019-07-01 23:53:01 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 //DECLARATION
 var express 	= require('express');
 var bodyParser 	= require('body-parser');
+var session     = require('express-session');
 var signup 		= require('./controller/signup');
 var login 		= require('./controller/login');
+var logout      = require('./controller/logout');
 var admin 		= require('./controller/admin');
 var volunteer 		= require('./controller/volunteer');
+var multer = require('multer');
+var path = require('path');
 var app = express();
 
 
 //CONFIGURATION
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
+app.use('/storage', express.static('storage'));
 
 
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(session({secret:'hhdhdhdhd', saveUninitialized: true, resave: false}));
 
 //app.use(bodyParser.json());
 
@@ -24,11 +30,12 @@ app.use(bodyParser.urlencoded({
 //ROUTING
 app.use('/signup', signup);
 app.use('/', login);
+app.use('/logout',logout);
 app.use('/admin', admin);
 app.use('/volunteer', volunteer);
-app.get('/index', function(request, response){
+/*app.get('/index', function(request, response){
 	response.render('index');
-});
+});*/
 
 
 //SERVER STARTUP
