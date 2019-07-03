83cc03405622af01a23ff7437a285ea49243c2bf
NajiaBarota <43447008+NajiaBarota@users.noreply.github.com>
2019-07-03 16:42:35 +0600

    Update admin.js

--- controller/admin.js
+++ controller/admin.js
@@ -44,11 +44,53 @@ router.get('/accept/:username', function(request, response){
 	
 });
 
+// get notice
+
+router.get('/noticeupdated', function(request, response){
+
+
+       adminModel.getNotice(function(status){
+         response.render('admin/noticeupdated',{userList: status});
+        });
+
+	});
+
+// ends
+
+
+
 router.get('/notice', function(request, response){
 	
     response.render('admin/notice');
 	
 });
+
+
+router.get('/noticeupdated', function(request, response){
+	
+    response.render('admin/noticeupdated');
+	
+});
+
+// admin list
+ 
+router.get('/adminlist', function(request, response){
+  
+    adminModel.getAdminList(function(status){
+                       response.render('admin/adminlist',{userList: status});  
+                        });
+  
+});
+
+//end
+
+
+router.get('/listuser', function(request, response){
+  
+    response.render('admin/listuser');
+  
+});
+
 
 
 router.post('/notice', function(request, response){
@@ -64,6 +106,56 @@ router.post('/notice', function(request, response){
         });
 
 	});
+
+
+router.get('/noticeupdated/delete/:id', function(request, response){
+	
+    
+    	 var id= request.params.id;
+
+
+   
+	adminModel.deleteNotice(id, function(status){
+		//console.log(username);
+                       response.redirect('/admin/noticeupdated');  
+                        });
+      			//cons
+	
+});
+
+router.get('/noticeupdated/update/:id', function(request, response){
+	
+    
+    	 var id= request.params.id;
+
+
+   
+	adminModel.getNotice2(id, function(status){
+		console.log(status[0].post);
+
+                       response.render('admin/updatenotice', {status});  
+                        });
+      			//cons
+	
+});
+router.post('/noticeupdated/update/:id', function(request, response){
+	
+    
+    	 var user = {
+    	 	id : request.params.id,
+    	 	post : request.body.notice
+    	 }
+
+//console.log(user);
+   
+	adminModel.updateNotice(user, function(status){
+		          
+		          console.log(status);
+                       response.redirect('/admin');  
+                        });
+      			//cons
+	
+});
 
 module.exports = router;
 
