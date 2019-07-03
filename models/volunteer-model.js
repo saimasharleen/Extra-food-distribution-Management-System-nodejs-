1dea6c8aa08527dcea958e2bee92b559f60307bb
saimasharleen <saimasharleen0@gmail.com>
2019-07-04 05:51:11 +0600

    Update volunteer-model.js

--- models/volunteer-model.js
+++ models/volunteer-model.js
@@ -7,6 +7,30 @@ module.exports={
 			//console.log(result);
 			callback(result[0]);
 		});
+
+},
+ clear: function(id, callback){
+		var sql = "delete from ownerpost where id="+id;
+		db.execute(sql, function(status){
+			//console.log(result);
+			callback(status);
+		});
+	},
+
+   
+	getinfo: function(callback){
+		var sql = "select * from ownerpost";
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
+	},
+	getnotification: function(callback){
+		var sql = "select * from ownerpost";
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
 	},
 updatedata: function(user, callback){
 	var sql = "UPDATE user set firstname=?, lastname=?, email=?, phoneno=?, area=? where username=?";
