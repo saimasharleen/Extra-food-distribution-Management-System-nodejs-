7efec5536578d4579b0456c15dee598e8996b96d
Tahmid Hossain <46225807+Optirius@users.noreply.github.com>
2019-07-05 21:46:52 +0600

    Add files via upload

--- controller/owner.js
+++ controller/owner.js
@@ -1,7 +1,20 @@
 var express = require('express');
-var router = express.Router();
+var multer  = require('multer');
 var mysql = require('mysql');
+var path    = require('path');
 var ownerModel = require.main.require('./models/owner-model');
+
+var router = express.Router();
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'extrafood'
+});
+
+connection.connect();
 
 router.get('/', function(request, response){
 	response.render('owner/home');
@@ -34,13 +47,6 @@ router.get('/posts/edit/:id', function(req, res){
 });
 
 
-router.get('/notification', function(request, response){
-	response.render('owner/notification');
-});
-
-router.get('/profile', function(request, response){
-	response.render('owner/profile');
-});
 
 router.get('/ranking', function(request, response){
 	response.render('owner/ranking');
@@ -95,6 +101,168 @@ router.get('/posts/delete/:id', function(req, res){
 			res.send('Error!.. try again...');
 		}
 	});
+});
+
+
+
+
+router.get('/notification', function(request, response){
+	response.render('owner/notification');
+});
+
+
+
+
+
+
+
+router.get('/profile', function(request, response){
+  user = request.session.un;
+ownerModel.get(user, function(status){
+  if(request.session.un != ""){
+      response.render('owner/profile',{userList:status});
+  }else{
+    response.redirect('/');
+}
+      });   
+});
+
+
+
+
+
+
+router.get('/editdata', function(request, response){
+  
+  user = request.session.un;
+ownerModel.get(user, function(status){
+      //console.log(status);
+      if(request.session.un != ""){
+    console.log(request.session.un);
+     response.render('owner/editdata',{userList:status});
+  }else{
+    response.redirect('/login');
+}
+        //response.render('volunteer/editdata',{userList:status});
+      }); 
+});
+
+
+
+
+
+
+router.post('/editdata',function(request, response){
+      var user={
+        username  : request.session.un,
+        firstname : request.body.firstname,
+        lastname  : request.body.lastname,
+        email     : request.body.email,
+        phoneno   : request.body.phoneno,
+        area      : request.body.area
+      };
+      console.log(user);
+      ownerModel.updatedata(user, function(status){
+
+        if(status == true){
+           if(request.session.un != ""){
+    console.log(request.session.un);
+     response.redirect('/owner/editprofile');
+  }else{
+    response.redirect('/');
+}
+          //response.redirect('/volunteer/editprofile');
+        }else{
+          response.send('Error in adding information ');
+        }
+      });
+  
+});
+
+
+
+
+
+
+router.get('/editprofile', function(request, response){
+   user = request.session.un;
+ownerModel.get(user, function(status){
+      //console.log(status);
+      if(request.session.un != ""){
+    console.log(request.session.un);
+     response.render('owner/editprofile',{userList:status});
+  }else{
+    response.redirect('/login');
+}
+        //response.render('volunteer/editprofile',{userList:status});
+      }); 
+});
+
+
+var storage = multer.diskStorage({
+  destination: './storage/',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname));
+  }
+});
+
+var upload = multer({
+  storage: storage
+}).single('photos');
+
+
+router.get('/editprofile', function(request, response){
+   user = request.session.un;
+volunteerModel.get(user, function(status){
+      //console.log(status);
+      if(request.session.un != ""){
+    console.log(request.session.un);
+     response.render('volunteer/editprofile',{userList:status});
+  }else{
+    response.redirect('/login');
+}
+        //response.render('volunteer/editprofile',{userList:status});
+      }); 
+});
+
+
+
+
+
+router.post('/editprofile', function(request, response){
+
+  var un  =  request.session.un;
+
+upload(request, response, function(err){
+  if(err){
+    response.redirect('owner/editprofile');
+  }else{
+    //console.log(request.file);
+    //console.log(request.file.filename);
+    
+      var user={
+        username  : un,
+        photos    : request.file.filename 
+      };
+      console.log(user);
+      ownerModel.updatepost(user, function(status){
+
+        if(status == true){
+          if(status == true){
+           if(request.session.un != ""){
+    console.log(request.session.un);
+     response.redirect('/owner/profile');
+  }else{
+    response.redirect('/');
+}
+          //response.redirect('/volunteer/profile');
+        }else{
+          response.send('Error in adding pic');
+        }
+  }
+});
+}
+});
 });
 
 
--- models/owner-model.js
+++ models/owner-model.js
@@ -1,8 +1,29 @@
 var db = require('./db');
 
 module.exports={
+	
+	get: function(userId, callback){
+		var sql = "select * from user where username=?";
+		db.getResult(sql, [userId], function(result){
+			//console.log(result);
+			callback(result[0]);
+		});
 
+},
 
+updatedata: function(user, callback){
+	var sql = "UPDATE user set firstname=?, lastname=?, email=?, phoneno=?, area=? where username=?";
+	db.execute(sql, [user.firstname, user.lastname, user.email, user.phoneno, user.area, user.username], function(status){
+			callback(status);
+		});
+},
+updatepost: function(user, callback){
+		var sql = "UPDATE user set u_img=? where username=?";
+		db.execute(sql, [user.photos, user.username], function(status){
+			callback(status);
+		});
+
+},
 	getAll: function(user, callback){
 		console.log(user);
 		var sql = "select * from ownerpost where username =?";
