1b07a1a3555d0ab8b3ae47b7e32080936e7150f5
NajiaBarota <43447008+NajiaBarota@users.noreply.github.com>
2019-07-06 16:40:13 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
 //DECLARATION
 var express 	= require('express');
 var bodyParser 	= require('body-parser');
-//var home 		= require('./controllers/home');
+var session     = require('express-session');
 var signup 		= require('./controller/signup');
+var login 		= require('./controller/login');
+var logout      = require('./controller/logout');
 var admin 		= require('./controller/admin');
+var superadmin 		= require('./controller/superadmin');
+var volunteer 		= require('./controller/volunteer');
+var  mysql=require('mysql');
+var multer = require('multer');
+var path = require('path');
+
 var app = express();
+
+
 
 
 //CONFIGURATION
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
+app.use('/storage', express.static('storage'));
 
 
 //MIDDLEWARE
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({secret:'hhdhdhdhd', saveUninitialized: true, resave: false}));
+
+
+//app.use(bodyParser.json());
 
 
 //ROUTING
 app.use('/signup', signup);
+app.use('/', login);
+app.use('/logout',logout);
 app.use('/admin', admin);
-/*app.use('/home', home);*/
-app.get('/', function(request, response){
-	/*response.send('Hello from expressJs');*/
+app.use('/superadmin', superadmin);
+app.use('/volunteer', volunteer);
+/*app.get('/index', function(request, response){
 	response.render('index');
-});
+});*/
+
 
 
 //SERVER STARTUP
 app.listen(3000, function(){
-	console.log("Server startead at 3000...");
+	console.log("Server started at 3000...");
 });
-
 
---
+++ models/volunteer-model.js
@@ -0,0 +1,81 @@
+var db = require('./db');
+
+module.exports={
+	get: function(userId, callback){
+		var sql = "select * from user where username=?";
+		db.getResult(sql, [userId], function(result){
+			//console.log(result);
+			callback(result[0]);
+		});
+
+},
+ clear: function(id, callback){
+		var sql = "delete from ownerpost where id="+id;
+		db.execute(sql, function(status){
+			//console.log(result);
+			callback(status);
+		});
+	},
+    
+    request: function(id, callback){
+		var sql = "UPDATE ownerpost set reqid='1' where id=?";
+
+		db.execute(sql, [id], function(status){
+			callback(status);
+		});
+	},
+   getVolunteerList: function(callback){
+		var sql = "select * from userlogin where usertype='volunteer'" ;
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
+	},
+   getEventList: function(callback){
+		var sql = "select * from eventmanage" ;
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
+	},
+	getvote: function(callback){
+		var sql = "select * from ownerpost  ";
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
+	},
+	getinfo: function(callback){
+		var sql = "select * from ownerpost where reqid= 0 ";
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
+	},
+	getnotification: function(callback){
+		var sql = "select * from ownerpost";
+		db.getResult(sql, [], function(result){
+			//console.log(result);
+			callback(result);
+		});
+	},
+	updatevote: function(user, callback){
+	var sql = "UPDATE ownerpost set vote = ? where id=?";
+	db.execute(sql, [user.rating, user.id], function(status){
+			callback(status);
+		});
+},
+updatedata: function(user, callback){
+	var sql = "UPDATE user set firstname=?, lastname=?, email=?, phoneno=?, area=? where username=?";
+	db.execute(sql, [user.firstname, user.lastname, user.email, user.phoneno, user.area, user.username], function(status){
+			callback(status);
+		});
+},
+updatepost: function(user, callback){
+		var sql = "UPDATE user set u_img=? where username=?";
+		db.execute(sql, [user.photos, user.username], function(status){
+			callback(status);
+		});
+
+}
+}
